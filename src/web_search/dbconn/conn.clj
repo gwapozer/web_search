592d2e9eb5a67b055ed22e8f20881d37b8a184ab
gwapozer <45476238+gwapozer@users.noreply.github.com>
2019-02-03 13:20:10 -0800

    Add files via upload

--- src/web_search/dbconn/conn.clj
+++ src/web_search/dbconn/conn.clj
@@ -1,13 +1,30 @@
 (ns web-search.dbconn.conn
-  (:require [cljhelpers.plugin :as pl])
+  (:require [cljhelpers.plugin :as pl]
+            [clojure.java.io :as io]
+            [cljhelpers.file :refer :all]
+            [cljhelpers.eval-obj :as oe]
+            [widget.evalobj-gui :as egui])
   )
 
 (defn db-spec
   []
-  (pl/add-to-classpath "/Users/tester/Applications/h2/bin/h2-1.4.197.jar")
 
-  {:dbtype "h2"
-   :dbname "/Users/tester/h2-database/my-db"
-   :user "sa"
-   :password ""
-   })
+  (let [item (clojure.java.io/resource "prod.edn")
+        my-config (readEdnFile item)
+        db-driver (clojure.java.io/resource "mysql-connector-java-5.1.7-bin.jar")
+        curr-driver-path (.getPath db-driver)
+        hasJar? (clojure.string/index-of (str curr-driver-path) (str (:jar-name my-config)))
+        driver-path (cond (nil? hasJar?) curr-driver-path
+                         :else  (str (-> (java.io.File. "") .getCanonicalPath) "/db-drivers/mysql-connector-java-5.1.7-bin.jar"))]
+
+    (pl/add-to-classpath driver-path)
+    ;(pl/add-to-classpath "/Users/gwapozer/Applications/mysql-connector-java-5.1.7/mysql-connector-java-5.1.7-bin.jar")
+    (:db-spec my-config)
+    )
+
+  ;{:dbtype "h2"
+  ; :dbname "/Users/gwapozer/h2-database/my-db"
+  ; :user "sa"
+  ; :password ""
+  ; }
+  )
